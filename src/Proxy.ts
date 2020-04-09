ca14ef77efd0bf7e277e8571bd69fbccc384a3ad
kovi10 <kovagomate.email@gmail.com>
2020-04-09 22:08:47 +0200

    nagyHF

--- src/Proxy.ts
+++ src/Proxy.ts
@@ -20,7 +20,7 @@ export class Proxy extends EventProducer<ProxyEventMap> {
         });
         this.ws.addEventListener("message", e => {
             //console.log("Hello");
-            console.log(e.data); //
+            console.log(e.data); 
             let p = <IncomingPacket>JSON.parse(e.data);
             switch (p.type) {
                 case "error":
